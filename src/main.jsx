bf753b5f1422bcf46ebbebcd266657a1dc0bb098
Hepi <121499922+Gamihepi@users.noreply.github.com>
2026-02-06 14:33:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { UserContextProvider } from './context/UserContext.jsx';
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
 // export const server = "https://e-learning-server-e95j.onrender.com";
-export const server = "https://e-learning-server-1-h7di.onrender.com"
+export const server = "https://e-learning-server-1-h7di.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
